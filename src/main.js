90b526a85b60367004e7306864ef34529e161ccb
Pogaku Shahazad <Pogaku.Shahazad@cerner.com>
2019-01-30 18:33:49 +0530

    added proxy api

--- src/main.js
+++ src/main.js
@@ -41,7 +41,7 @@ class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            'username': 'PM74543', 'password': '', isLoggedIn: false, isUserValid: false, isAdmin: false,
+            'username': 'PM1235gf', 'password': '', isLoggedIn: false, isUserValid: false, isAdmin: false,
             'items': [], 'hash': '', 'id': '', hostIP: config.hostIP, port: config.port, channelName: config.channelName, chaincodeName: config.chaincodeName, peerName: config.peerName,
             'auth': config.authToken,
             'toutput': [], 'foutput': [], view: false, disableHashInput: false,disableFilterSelection: false,
@@ -130,7 +130,7 @@ class Main extends Component {
             },
         }
         // eslint-disable-next-line
-        fetch('http://' + this.state.hostIP + ':' + this.state.port + '' + '/channels/' + this.state.channelName + '/chaincodes/' + this.state.chaincodeName + '?peer=' + this.state.peerName + '&fcn=queryCustom&args=%5B%22%7B%5C%22selector%5C%22:%7B%5C%22_rev%5C%22:%5C%22' + this.state.hash + '%5C%22,%5C%22payerId%5C%22:%5C%22' + this.state.username + '%5C%22%7D%7D%22%5D', config)
+        fetch('/api' + '/channels/' + this.state.channelName + '/chaincodes/' + this.state.chaincodeName + '?peer=' + this.state.peerName + '&fcn=queryCustom&args=%5B%22%7B%5C%22selector%5C%22:%7B%5C%22_rev%5C%22:%5C%22' + this.state.hash + '%5C%22,%5C%22payerId%5C%22:%5C%22' + this.state.username + '%5C%22%7D%7D%22%5D', config)
             .then(response => response.json())
             .then(response => {
                 if (JSON.stringify(response) === '[]') {
@@ -154,7 +154,7 @@ class Main extends Component {
             },
         }
         // eslint-disable-next-line
-        fetch('http://' + this.state.hostIP + ':' + this.state.port + '' + '/channels/' + this.state.channelName + '/chaincodes/' + this.state.chaincodeName + '?peer=' + this.state.peerName + '&fcn=isValid&args=%5B%22hash%22,%22' + this.state.hash + '%22%5D', config)
+        fetch('/api' + '/channels/' + this.state.channelName + '/chaincodes/' + this.state.chaincodeName + '?peer=' + this.state.peerName + '&fcn=isValid&args=%5B%22hash%22,%22' + this.state.hash + '%22%5D', config)
             .then(response => response.text())
             .then(response => {
                 if (response.length === 0 && response[0] !== 'E') {
@@ -175,7 +175,9 @@ class Main extends Component {
             },
         }
         // eslint-disable-next-line
-        fetch(/* 'http://' + this.state.hostIP + ':' + this.state.port + '' */'/api' + '/channels/' + this.state.channelName + '/chaincodes/' + this.state.chaincodeName + '?peer=' + this.state.peerName + '&fcn=isValid&args=%5B%22payer%22,%22' + this.state.username + '%22%5D', config)
+        //fetch(/* 'http://' + this.state.hostIP  + ':' + this.state.port + '' */'/api' + '/channels/' + this.state.channelName + '/chaincodes/' + this.state.chaincodeName + '?peer=' + this.state.peerName + '&fcn=isValid&args=%5B%22payer%22,%22' + this.state.username + '%22%5D', config)
+
+        fetch(/* '/api' */'/api' + '/channels/' + this.state.channelName + '/chaincodes/' + this.state.chaincodeName + '?peer=' + this.state.peerName + '&fcn=isValid&args=%5B%22payer%22,%22' + this.state.username + '%22%5D', config)
             .then(response => response.text())
             .then(response => {
                 if (response.length === 0 && response[0] !== 'E') {
@@ -220,7 +222,7 @@ class Main extends Component {
             },
         }
         // eslint-disable-next-line
-        fetch('http://' + this.state.hostIP + ':' + this.state.port + '' + '/channels/' + this.state.channelName + '/chaincodes/' + this.state.chaincodeName + '?peer=' + this.state.peerName + '&fcn=isValid&args=%5B%22hash%22,%22' + this.state.hash + '%22%5D', config)
+        fetch('/api' + '/channels/' + this.state.channelName + '/chaincodes/' + this.state.chaincodeName + '?peer=' + this.state.peerName + '&fcn=isValid&args=%5B%22hash%22,%22' + this.state.hash + '%22%5D', config)
             .then(response => response.text())
             .then(response => {
                this.setState({ submitersFilters : response.fhir})
@@ -747,7 +749,7 @@ class Main extends Component {
             <div>
                 
 
-{result}
+{mainPage}
 
                 
             </div>
